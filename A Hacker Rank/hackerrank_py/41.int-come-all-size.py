a6e6dfa5fcb52a4b14c156c35e9f908dc9d3ff4c
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-11-14 12:20:05 +0600

    fix: solved python-integers-come-in-all-sizes problem from hacker rank.

---
+++ A Hacker Rank/hackerrank_py/41.int-come-all-size.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(str((a**b)+(c**d)))
+
+"""
+Sample Input
+
+9
+29
+7
+27
+Sample Output
+
+4710194409608608369201743232 
+"""
