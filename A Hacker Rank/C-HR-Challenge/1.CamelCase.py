a650bb13eecad3bfaf602a977684b1d2f34fde1d
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-11-17 10:19:01 +0600

    fix: done CamelCase challenge using python.

---
+++ A Hacker Rank/C-HR-Challenge/1.CamelCase.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    words_number = 1
+    for alphabet in s:
+        if alphabet.isupper():
+            words_number += 1 
+    return words_number
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+"""
+Sample Input
+
+saveChangesInTheEditor
+Sample Output
+
+5
+"""
