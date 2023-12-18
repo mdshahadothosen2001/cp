f10e45e19d46be29c77593f3b28c39b5cc1d9047
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-12-18 20:51:42 +0600

    fix: solved majority element

---
+++ D Leetcode [--job/99days/4.majority-element.py
@@ -0,0 +1,33 @@
+from typing import List
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+test = Solution()
+print(test.majorityElement([2,2,1,1,1,2,2]))
+
+"""
+Example 1:
+
+Input: nums = [3,2,3]
+Output: [3]
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+"""
