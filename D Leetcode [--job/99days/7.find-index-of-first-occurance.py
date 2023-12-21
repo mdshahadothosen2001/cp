85db04310abfc11553c99b6c03484906db8205ba
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-12-21 07:49:26 +0600

    fix: solved find index of first occurance in a string

---
+++ D Leetcode [--job/99days/7.find-index-of-first-occurance.py
@@ -0,0 +1,22 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)):
+            if haystack[i] == needle[0]:
+                if haystack[i:i+len(needle)] == needle:
+                    return i 
+        return -1
+
+"""
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+"""
