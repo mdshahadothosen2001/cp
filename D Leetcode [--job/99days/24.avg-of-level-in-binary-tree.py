21e4776296daec28a7456cfedb7598cdc86e9464
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-01-10 12:31:18 +0600

    fix: solved avg-of-level-in-binary-tree
    Calculate average number of nodes each level in binary tree

---
+++ D Leetcode [--job/99days/24.avg-of-level-in-binary-tree.py
@@ -0,0 +1,69 @@
+from typing import Optional, List
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        if not root:
+            return []
+        i = -1
+        nodes_list = []
+        nodes_level_list = []
+        nodes = deque([(root, 0)])
+        current_level = -1
+        while nodes:
+            current_node, level = nodes.popleft()
+            if level > current_level:
+                nodes_list.append(nodes_level_list)
+                nodes_level_list = []
+                current_level = level
+            nodes_level_list.append(current_node.val)
+            
+            if current_node.left:
+                nodes.append((current_node.left, level+1))
+            if current_node.right:
+                nodes.append((current_node.right, level+1))
+        nodes_list.append(nodes_level_list)
+        
+        avg = []
+        for node in nodes_list[1:]:
+            number_of_node = len(node)
+            sum_of_node = sum(node)
+            div_of_node = sum_of_node / number_of_node
+            avg.append(div_of_node)
+        return avg
+
+
+head = TreeNode(1)
+head.left = TreeNode(2)
+head.right = TreeNode(3)
+
+head.left.left = TreeNode(4)
+head.left.right = TreeNode(5)
+
+
+
+head.right.left = TreeNode(6)
+head.right.right = TreeNode(7)
+
+head.right.right.left = TreeNode(9)
+head.right.right.right = TreeNode(10)
+
+checker = Solution()
+result = checker.averageOfLevels(head)
+# expected output: [1.0, 2.5, 5.5, 9.5]
+print(result)
+
+# Example 1:
+# Input: root = [3,9,20,null,null,15,7]
+# Output: [3.00000,14.50000,11.00000]
+# Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+# Hence return [3, 14.5, 11].
+
+# Example 2:
+# Input: root = [3,9,20,15,7]
+# Output: [3.00000,14.50000,11.00000]
