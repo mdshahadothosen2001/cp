b6e8875c7321484a5ac453d85315377abfcad8f9
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-04-10 15:33:55 +0600

    fix: kids-with-the-greatest-number-of-candies

---
+++ D Leetcode [--job/99days/39.greatest-candies.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candy = max(candies)
+        results = []
+        for candy in candies:
+            if candy+extraCandies>=max_candy:
+                results += [True]
+            else:
+                results += [False]
+        return results
+    
+checker = Solution()
+candies = [1,2,4,3,2]
+result = checker.kidsWithCandies(candies, 2)
+print(result)
+
+
+# Example 1:
+# Input: candies = [2,3,5,1,3], extraCandies = 3
+# Output: [true,true,true,false,true] 
+# Explanation: If you give all extraCandies to:
+# - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+# - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+# - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+# - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+# - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+# Example 2:
+# Input: candies = [4,2,1,1,2], extraCandies = 1
+# Output: [true,false,false,false,false] 
+# Explanation: There is only 1 extra candy.
+# Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+
+# Example 3:
+# Input: candies = [12,1,12], extraCandies = 10
+# Output: [true,false,true]
