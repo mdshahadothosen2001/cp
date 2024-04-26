f44eb69d5ab0230c0e8fe71882d2300fb576bb04
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-04-26 07:15:49 +0600

    fix: solved the problem about find number of good pairs

---
+++ D Leetcode [--job/99days/50.number-of-good-pairs.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        good_pairs = 0
+        length = len(nums)
+        for i in range(length):
+            for j in range(i+1,length):
+                if nums[i] == nums[j]:
+                    good_pairs += 1
+        return good_pairs
+
+# Example 1:
+
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+# Example 2:
+# Input: nums = [1,1,1,1]
+# Output: 6
+# Explanation: Each pair in the array are good.
+# Example 3:
+
+# Input: nums = [1,2,3]
+# Output: 0
