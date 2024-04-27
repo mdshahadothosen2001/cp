782d1d3cea8c6219ea4159c756daeec375d3af6e
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-04-27 08:07:04 +0600

    fix: solved find words containing character problem

---
+++ D Leetcode [--job/99days/53.find-words-containing-character.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        indices = []
+        for i, item in enumerate(words):
+            if x in item:
+                indices += [i]
+        return indices
+
+
+# Example 1:
+# Input: words = ["leet","code"], x = "e"
+# Output: [0,1]
+# Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+
+# Example 2:
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+# Output: [0,2]
+# Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+
+# Example 3:
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+# Output: []
+# Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
