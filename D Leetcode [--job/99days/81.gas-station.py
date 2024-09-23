e2041fd7aea17cd12ea73707d8f3ce19a2981c9e
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-09-23 07:56:22 +0600

    fix: solved gas station traveling around the circuit once in the clockwise direction

---
+++ D Leetcode [--job/99days/81.gas-station.py
@@ -0,0 +1,50 @@
+# 134. Gas Station
+
+from typing import List
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        tank = 0
+        start_station = 0
+
+        for i in range(len(gas)):
+            tank += (gas[i] - cost[i])
+
+            if tank < 0:
+                tank = 0
+                start_station = i + 1
+        return start_station
+
+
+# gas = [1,2,3,4,5]
+# cost = [3,4,5,1,2]
+# difference = [-2, -2, -2, 3, 3]
+# Here index 3 of difference is a position value, definately there is a solution to travel
+# you can travel around the circuit once in the clockwise direction from station 3 as index
+
+
+# Example 1:
+# Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+# Output: 3
+# Explanation:
+# Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+# Travel to station 4. Your tank = 4 - 1 + 5 = 8
+# Travel to station 0. Your tank = 8 - 2 + 1 = 7
+# Travel to station 1. Your tank = 7 - 3 + 2 = 6
+# Travel to station 2. Your tank = 6 - 4 + 3 = 5
+# Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+# Therefore, return 3 as the starting index.
+
+# Example 2:
+# Input: gas = [2,3,4], cost = [3,4,3]
+# Output: -1
+# Explanation:
+# You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+# Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+# Travel to station 0. Your tank = 4 - 3 + 2 = 3
+# Travel to station 1. Your tank = 3 - 3 + 3 = 3
+# You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+# Therefore, you can't travel around the circuit once no matter where you start.
