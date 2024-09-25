19c1b94ec49df5ed1c390cfc32e0a65d3e267606
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-09-25 19:32:29 +0600

    fix: solved calculating maximum amount of rain water

---
+++ D Leetcode [--job/99days/83.trapping-rain-water.py
@@ -0,0 +1,28 @@
+from typing import List 
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left, right = 0, len(height)-1
+        max_left, max_right = height[left], height[right]
+        water = 0
+        while left<right:
+            if max_left < max_right:
+                left += 1
+                max_left = max(max_left, height[left])
+                water += max_left - height[left]
+            else:
+                right -= 1
+                max_right = max(max_right, height[right])
+                water += max_right - height[right]
+        return water
+    
+
+
+# Example 1:
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+# Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+# Example 2:
+# Input: height = [4,2,0,3,2,5]
+# Output: 9
